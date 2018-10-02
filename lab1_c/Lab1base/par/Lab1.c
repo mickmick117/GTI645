b7300bb9ec669abfa7b97e2e4f5061d5c783e038
raphaeldoye <raphaeldoye@hotmail.fr>
2018-10-02 00:21:55 -0400

    parallel test

--- lab1_c/Lab1base/par/Lab1.c
+++ lab1_c/Lab1base/par/Lab1.c
@@ -6,6 +6,31 @@
 
 #define WAIT_TIME 1000
 
+int matrix[8][8];
+
+
+void initMatrixValues(int initValue)
+{
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			matrix[i][j] = initValue;
+		}
+	}
+}
+
+void printMatrix()
+{
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			printf("%d,",matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
 
 int main(int argc, char* argv[])
 {
@@ -18,7 +43,7 @@ int main(int argc, char* argv[])
 
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	printf("Je suis rank %d\n", rank);
+	//printf("Je suis rank %d\n", rank);
 
 	int nbProbleme = atoi(argv[1]);
 	int initValue = atoi(argv[2]);
@@ -27,7 +52,22 @@ int main(int argc, char* argv[])
 	
 	clock_t begin = clock();
 	// Faire le tp
-	
+	initMatrixValues(initValue);
+
+	for (int k = 1; k <= nbItterations; k++)
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				usleep(WAIT_TIME);
+				printf("rank %i working on element %i\n", rank, i);
+				matrix[i][j] = matrix[i][j] + (i + j) * k;
+			}
+		}
+	}
+
+	printMatrix();
 
 	// Un seul process doit faire le print.
 	if (rank == 0) {
