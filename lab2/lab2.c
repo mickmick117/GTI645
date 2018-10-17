15171415a41ce256aa8a5e2929c0c3ddcda780bd
raphaeldoye <raphaeldoye@hotmail.fr>
2018-10-16 21:26:25 -0400

    fuck

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -109,6 +109,7 @@ void probleme1Par(int initValue, int iteration)
 				if(thread <= nbThreadOneElementMore)
 				{
 					nbElementsEach++;
+					printf("%d, \n",nbElementsEach);
 				}
 				
 				for(int element = 0; element < nbElementsEach; element++)
